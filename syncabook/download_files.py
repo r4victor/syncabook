2956abdf0c4460b0154f56d73c67ca9ea3a390ed
Victor Skvortsov <vds003@gmail.com>
2020-05-25 16:42:58 +0500

    Download files from the synclibrivox repo

--- syncabook/download_files.py
+++ syncabook/download_files.py
@@ -1,4 +1,5 @@
-import os
+import json
+import os.path
 import re
 import urllib.parse
 import urllib.request
@@ -8,32 +9,55 @@ from bs4 import BeautifulSoup
 import progressbar
 
 
+# synclibrivox repo parameters
+GITHUB_CONTENTS_URL = f'https://api.github.com/repos/r4victor/synclibrivox/contents/'
+MAPPING_FILE = 'map.json'
+BOOKS_DIR = 'books'
+
+
 def download_files(librivox_url, output_dir):
+    """
+    Downloads files needed to create an ebook.
+
+    The downloading process looks as follows:
+    Download files from the synclibrivox repository.
+    If a book is not found there, download plaintext transcript from gutenberg.org.
+    Download audio files from librivox.org.
+    """
     os.makedirs(output_dir, exist_ok=True)
 
     with urllib.request.urlopen(librivox_url) as f:
         librivox_soup = BeautifulSoup(f.read(), 'lxml')
 
-    gutenberg_link = librivox_soup.find('a', {'href': re.compile(r'http://www.gutenberg.org/.*')})
-    if gutenberg_link is None:
-        print('Link to the gutenberg.org is not found. Text will not be downloaded.')
+    print('Searching for files in the synclibrivox repository...')
+    found = _download_synclibrivox_files(librivox_url, output_dir)
+    if found:
+        print(
+            f'This book has been found in the synclibrivox repository.'
+            f' Corresponding files have been downloaded to {output_dir}.'
+            )
     else:
-        print('Downloading text...')
-        gutenberg_url = gutenberg_link['href']
+        print(
+            f'The synclibrivox repository doesn\'t contain this book.\n'
+            'Downloading text from gutenberg.org...'
+        )
+        gutenberg_link = librivox_soup.find('a', {'href': re.compile(r'http://www.gutenberg.org/.*')})
+        if gutenberg_link is None:
+            print('Link to the gutenberg.org is not found. Text won\'t be downloaded.')
+        else:
+            gutenberg_url = gutenberg_link['href']
 
-        with urllib.request.urlopen(gutenberg_url) as f:
-            gutenberg_soup = BeautifulSoup(f.read(), 'lxml')
+            with urllib.request.urlopen(gutenberg_url) as f:
+                gutenberg_soup = BeautifulSoup(f.read(), 'lxml')
 
-        text_relative_url = gutenberg_soup.find('a', {'type': re.compile(r'text/plain.*')})['href']
-        text_absolute_url = urllib.parse.urljoin('http://www.gutenberg.org/', text_relative_url)
-        text_path = os.path.join(output_dir, 'text.txt')
+            text_relative_url = gutenberg_soup.find('a', {'type': re.compile(r'text/plain.*')})['href']
+            text_absolute_url = urllib.parse.urljoin('http://www.gutenberg.org/', text_relative_url)
+            text_path = os.path.join(output_dir, 'text.txt')
 
-        urllib.request.urlretrieve(text_absolute_url, text_path, reporthook=ProgressBar())
+            urllib.request.urlretrieve(text_absolute_url, text_path, reporthook=ProgressBar())
+            print(f'Text has been downloaded and saved as {text_path}')
 
-        print(f'Text has been downloaded and saved as {text_path}')
-
-    print('Downloading audio...')
-
+    print('Downloading audio files...')
     book_url = librivox_soup.find('a', class_='book-download-btn')['href']
     local_filename, _ = urllib.request.urlretrieve(book_url, reporthook=ProgressBar())
 
@@ -41,8 +65,7 @@ def download_files(librivox_url, output_dir):
 
     with ZipFile(local_filename) as z:
         z.extractall(path=audio_dir)
-
-    print(f'Audio has been downloaded and saved to {audio_dir}')
+    print(f'Audio files have been downloaded to {audio_dir}')
 
 
 class ProgressBar():
@@ -60,3 +83,48 @@ class ProgressBar():
             self.pbar.update(downloaded)
         else:
             self.pbar.finish()
+
+
+def _download_synclibrivox_files(librivox_url, output_dir):
+    """
+    Downloads files from synclibrivox repository for a book with corresponding `librivox_url`.
+    If a book is not found is not found in the repository, returns False.
+    Otherwise, returns True.
+    """
+    book_dir = _get_book_dir(librivox_url)
+    if book_dir is None:
+        return False
+    
+    book_path = os.path.join(BOOKS_DIR, book_dir)
+    _download_github_directory(book_path, relative_to=book_path, output_dir=output_dir)
+    return True
+
+
+def _download_github_directory(path, relative_to='', output_dir=''):
+    url = urllib.parse.urljoin(GITHUB_CONTENTS_URL, path)
+    with urllib.request.urlopen(url) as f:
+        contents = json.loads(f.read())
+
+    os.makedirs(os.path.join(output_dir, os.path.relpath(path, relative_to)), exist_ok=True)
+
+    for content in contents:
+        if content['type'] == 'file':
+            urllib.request.urlretrieve(
+                content['download_url'],
+                os.path.join(output_dir, os.path.relpath(content['path'], relative_to))
+            )
+        elif content['type'] == 'dir':
+            _download_github_directory(content['path'], relative_to=relative_to, output_dir=output_dir)
+
+
+def _get_book_dir(librivox_url):
+    return json.loads(_get_github_file_contents(MAPPING_FILE)).get(librivox_url)
+
+
+def _get_github_file_contents(file_path):
+    file_url = urllib.parse.urljoin(GITHUB_CONTENTS_URL, file_path)
+    with urllib.request.urlopen(file_url) as f:
+        download_url = json.loads(f.read())['download_url']
+
+    with urllib.request.urlopen(download_url) as f:
+        return f.read()
